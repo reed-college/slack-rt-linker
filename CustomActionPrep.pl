870926e55f6672e923d89f667e07e73aabddd447
tackittj <tackittj@reed.edu>
2025-11-21 13:54:59 -0800

    Update CustomActionPrep.pl
    Custom Action Prep code is no longer used.

--- CustomActionPrep.pl
+++ CustomActionPrep.pl
@@ -1,11 +1,1 @@
-# clear out the timestamp cf to allow messages to post to new channels when a ticket is moved to a different queue that also has this scrip enabled.
-# but not if transaction is On Owner Change
-my $field = $self->TransactionObj->Field;
-my $type = $self->TransactionObj->Type;
-
-# On Owner Change
-unless ( $field eq "Owner" && $type eq "Set" ){
-    $self->TicketObj->AddCustomFieldValue( Field =>"slack_timestamp", Value => '' );
-}
-
 return 1;
